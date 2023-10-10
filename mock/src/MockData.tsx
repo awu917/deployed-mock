795f722688a5ddcd845c709f434e946c8ddce88a
Fanny Vavrovsky <fanny-marie_vavrovsky@brown.edu>
2023-10-10 11:40:13 -0400

    chore: added csv data set

--- mock/src/MockData.tsx
+++ mock/src/MockData.tsx
@@ -6,3 +6,8 @@ viewHash.set("user/data/d20_csv", [["Campaign","Year","DM"],["Fantasy High: Fres
 ["The Unsleeping City", "2020", "Brennan Lee Mulligan"], ["Mentopolis", "2023", 
 "Brennan Lee Mulligan"], ["Court of Fey and Flowers", "2022", "Aabria Iyengar"]])
 
+viewHash.set("user/data/dininghall_csv", [["1","2","3","4","5","6"],["andrews",
+"peaches", "yakisoba", "apple juice", "portion", "11am"], ["ratty","bananas","soups", "coffee milk", "unlimited", "4pm"],
+["vdub","apples","falafel","mgp","unlimited","2pm"],["jo's", "no fruit", "impossible burger","peace tea","portion","8pm"], ["ivy room", "kiwi", 
+"smoothies","water","portion","3pm"]])
+// 1= Dining hall, 2=fruits, 3=dish, 4=drink, 5=unlimited/portion, 6=best hour
